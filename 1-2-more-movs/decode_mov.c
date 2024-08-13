0cf1002d91a54adb2bcf7a83f995ca976f18268e
hgrsd <daniel@hgrsd.nl>
2024-08-13 10:47:00 +0100

    1-2

---
+++ 1-2-more-movs/decode_mov.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+
+typedef enum {
+  AL,
+  AX,
+  CL,
+  CX,
+  DL,
+  DX,
+  BL,
+  BX,
+  AH,
+  SP,
+  CH,
+  BP,
+  DH,
+  SI,
+  BH,
+  DI,
+  UNKNOWN_REG
+} Reg;
+
+typedef enum {
+  MOV_RM_RM,
+  MOV_IM_REG,
+  UNKNOWN_OP,
+} Op;
+
+typedef enum {
+  MEM_NO_DISP,
+  MEM_DISP_8,
+  MEM_DISP_16,
+  REG,
+  UNKNOWN_MOD,
+} Mod;
+
+typedef struct {
+  Op opcode;
+  int D;
+  int W;
+  Mod mod;
+  Reg src;
+  Reg dst;
+} instruction ;
+
+Op parse_opcode(unsigned char b) {
+  switch (b) {
+  case 0x22:
+    return MOV_RM_RM;
+
+  default:
+    return UNKNOWN_OP;
+  }
+}
+
+Mod parse_mode(unsigned char b) {
+  switch (b) {
+  case 0b00:
+    return MEM_NO_DISP;
+  case 0b01:
+    return MEM_DISP_8;
+  case 0b10:
+    return MEM_DISP_16;
+  case 0b11:
+    return REG;
+  default:
+    return UNKNOWN_MOD;
+  }
+}
+
+Reg parse_register(int W, unsigned char b) {
+  switch (b) {
+  case 0b000:
+    return W ? AX : AL;
+  case 0b001:
+    return W ? CX : CL;
+  case 0b010:
+    return W ? DX : DL;
+  case 0b011:
+    return W ? BX : BL;
+  case 0b100:
+    return W ? SP : AH;
+  case 0b101:
+    return W ? BP : CH;
+  case 0b110:
+    return W ? SI : DH;
+  case 0b111:
+    return W ? DI : BH;
+  default:
+    return UNKNOWN_REG;
+  }
+}
+
+instruction parse_inst(const unsigned char buf[]) {
+  int W = buf[0] & 1;
+  int D = (buf[0] >> 1) & 1;
+  Op op = parse_opcode(buf[0] >> 2);
+
+  Reg src = parse_register(W, D ? (buf[1] & 7) : (buf[1] >> 3) & 7);
+  Reg dst = parse_register(W, D ? ((buf[1] >> 3) & 7) : buf[1] & 7);
+  Mod mod = parse_mode(buf[1] >> 6);
+
+  instruction ret = {
+      .opcode = op, .W = W, .D = D, .mod = mod, .dst = dst, .src = src};
+
+  return ret;
+}
+
+void print_reg(Reg *r) {
+  switch (*r) {
+  case AL:
+    printf("AL");
+    break;
+  case AX:
+    printf("AX");
+    break;
+  case CL:
+    printf("CL");
+    break;
+  case CX:
+    printf("CX");
+    break;
+  case DL:
+    printf("DL");
+    break;
+  case DX:
+    printf("DX");
+    break;
+  case BL:
+    printf("BL");
+    break;
+  case BX:
+    printf("BX");
+    break;
+  case AH:
+    printf("AH");
+    break;
+  case SP:
+    printf("SP");
+    break;
+  case CH:
+    printf("CH");
+    break;
+  case BP:
+    printf("BP");
+    break;
+  case DH:
+    printf("DH");
+    break;
+  case SI:
+    printf("SI");
+    break;
+  case BH:
+    printf("BH");
+    break;
+  case DI:
+    printf("DI");
+    break;
+  case UNKNOWN_REG:
+    printf("UNK");
+    break;
+  }
+}
+
+void print_instruction(instruction *inst) {
+  switch (inst->opcode) {
+  case MOV_RM_RM :
+  case MOV_IM_REG:
+    printf("MOV ");
+    break;
+  case UNKNOWN_OP:
+    printf("UNK ");
+    break;
+  }
+
+  print_reg(&inst->dst);
+  printf(", ");
+  print_reg(&inst->src);
+  printf("\n");
+}
+
+int main(int argc, char **argv) {
+  unsigned char buf[1000];
+  int len = 0;
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
+
+  FILE *f = fopen(argv[1], "r");
+  if (f == NULL) {
+    fprintf(stderr, "unable to read file %s\n", argv[1]);
+    return 1;
+  }
+
+  int c;
+  while ((c = getc(f)) != EOF) {
+    buf[len++] = c;
+  }
+
+  for (int i = 0; i < len; i += 2) {
+    printf("%d: %x %x > ", i, buf[i], buf[i + 1]);
+    unsigned char *ptr = buf + i;
+    instruction i = parse_inst(ptr);
+    print_instruction(&i);
+  }
+
+  return 0;
+}
